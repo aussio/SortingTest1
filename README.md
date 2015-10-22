15680f6e16b04caa0c0ffd19fe3709209b0a8c11
aussio <auss@wpengine.com>
2015-10-21 21:17:24 -0500

    Intial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple test to play with writing a **Merge Sort** or an **Insertion Sort** in Python!
+
+The tests are already written in `test_sorts.py` - just write the code in `sorts.py`!
---
+++ sorts.py
@@ -0,0 +1,15 @@
+def merge_sort(unsorted_list):
+    '''
+    Change me to actually sort "unsorted_list" via Merge Sort!
+    '''
+    return unsorted_list
+
+
+
+
+
+def insertion_sort(unsorted_list):
+    '''
+    Change me to actually sort "unsorted_list" via Insertion Sort!
+    '''
+    return unsorted_list
